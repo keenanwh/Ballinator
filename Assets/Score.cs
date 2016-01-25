dbb37ab4bbf4cb112a6ec81d1850c2a3db20e37d
keenman76 <keenman@gmail.com>
2016-01-25 13:09:29 -0500

    Initial checkin for Ballinator

---
+++ Assets/Score.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class Score : MonoBehaviour
+{
+    private int currentScore;
+    private TextMesh scoreText;
+
+    public void Reset()
+    {
+        currentScore = 0;
+    }
+
+    public void FellOff()
+    {
+        currentScore -= 2;
+    }
+
+    public void CollectedItem()
+    {
+        currentScore++;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        scoreText = GetComponent<TextMesh>();
+        scoreText.text = "0";
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+}
